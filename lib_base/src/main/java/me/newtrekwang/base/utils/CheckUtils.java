799ab06e1fe31c233e953576f08229e74a1bd0b0
WangJiaXing <408030208@qq.com>
2019-06-10 09:17:02 +0800

    1. 简化CheckUtils

--- lib_base/src/main/java/me/newtrekwang/base/utils/CheckUtils.java
+++ lib_base/src/main/java/me/newtrekwang/base/utils/CheckUtils.java
@@ -1,6 +1,8 @@
 package me.newtrekwang.base.utils;
 
-import androidx.annotation.Nullable;
+import java.lang.reflect.Method;
+
+import javax.annotation.Nullable;
 
 /**
  * @className CheckUtils
@@ -13,16 +15,68 @@ import androidx.annotation.Nullable;
 public final class CheckUtils {
 
     private CheckUtils(){}
+    /**
+     * 快速构造方法异常
+     * @param method
+     * @param message
+     * @param args
+     * @return
+     */
+    public static RuntimeException methodError(Method method, String message, Object... args){
+        return methodError(method,null,args);
+    }
 
     /**
-     * 检查空指针
+     * 快速构造方法异常
+     * @param method
+     * @param cause
+     * @param message
+     * @param args
+     * @return
+     */
+    public static RuntimeException methodError(Method method, @javax.annotation.Nullable Throwable cause, String message, Object... args){
+        message = String.format(message,args);
+        return new IllegalArgumentException(message
+                + "\n   for method "
+                + method.getDeclaringClass().getSimpleName()
+                + "."
+                +method.getName(),cause);
+    }
+
+    /**
+     * 快速构造参数异常
+     * @param method 方法
+     * @param cause
+     * @param p
+     * @param message
+     * @param args
+     * @return
+     */
+    public static RuntimeException parameterError(Method method, Throwable cause,int p,String message,Object... args){
+        return methodError(method,cause,message+"(parameter #"+(p+1)+")",args);
+    }
+
+    /**
+     * 快速构造参数异常
+     * @param method
+     * @param p
+     * @param message
+     * @param args
+     * @return
+     */
+    public static RuntimeException parameterError(Method method,int p,String message,Object... args){
+        return methodError(method,message+" (parameter #"+(p+1) + ")",args);
+    }
+
+    /**
+     * 检查空引用，如果为空，会抛空指针异常
      * @param object
      * @param message
      * @param <T>
      * @return
      */
-    public static <T> T checkNotNull(@Nullable T object, String message) {
-        if (object == null) {
+    public static <T> T checkNotNull(@Nullable T object, String message){
+        if (object == null){
             throw new NullPointerException(message);
         }
         return object;
